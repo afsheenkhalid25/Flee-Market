16d0a4814aef48e1396bce3842e2c1fe4dd7193b
afsheenkhalid25 <afsheen.kewt@gmail.com>
2015-10-02 01:28:41 +0500

    changes in map activity

--- FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/CreateMarket.java
+++ FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/CreateMarket.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class CreateMarket extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private String lat,lon,address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,13 @@ public class CreateMarket extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         Log.d("MAP......", "Ready");
         googleMap.setMyLocationEnabled(true);
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                lat= ""+latLng.latitude;
+                lon= ""+latLng.longitude;
+
+            }
+        });
     }
 }
