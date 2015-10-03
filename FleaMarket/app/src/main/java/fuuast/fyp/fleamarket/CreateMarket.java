71da7985840ff5273b8f3d9a477c5ee8c85c9348
Ay-Bee <abdulbst1994@gmail.com>
2015-10-03 17:17:06 +0500

    Create Market Completed 10/3/2015

--- FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/CreateMarket.java
+++ FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/CreateMarket.java
@@ -8,21 +8,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
 
 public class CreateMarket extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener, GoogleMap.OnMapClickListener{
 
     private String lat=null,lon=null,address=null,name=null;
     private EditText et_name;
     private Button create;
+    private MarketDataModel marketDataModel;
+    private UserDataModelSingleTon userDataModelSingleTon;
+    private Firebase firebase,markets;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_market);
+
+        Firebase.setAndroidContext(this);
+        firebase=new Firebase("https://flee-market.firebaseio.com/");
 
         et_name= (EditText) findViewById(R.id.createmarket_et_marketname);
         create= (Button) findViewById(R.id.createmarket_btn_create);
@@ -31,6 +44,9 @@ public class CreateMarket extends FragmentActivity implements OnMapReadyCallback
         et_name.setEnabled(false);
         create.setEnabled(false);
 
+        marketDataModel=new MarketDataModel();
+        userDataModelSingleTon=UserDataModelSingleTon.getInstance();
+
         Toast.makeText(this,"Please Wait For Map",Toast.LENGTH_LONG).show();
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.createmarket_map)).getMapAsync(this);
     }
@@ -38,14 +54,17 @@ public class CreateMarket extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Log.d("MAP......", "Ready");
-        googleMap.setMyLocationEnabled(true);
-        googleMap.setOnMapClickListener(this);
+        mMap=googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this);
         et_name.setEnabled(true);
         create.setEnabled(true);
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
         lat= ""+latLng.latitude;
         lon= ""+latLng.longitude;
         GetAddress objGetAddress=new GetAddress(CreateMarket.this,Double.parseDouble(lat),Double.parseDouble(lon));
@@ -61,10 +80,68 @@ public class CreateMarket extends FragmentActivity implements OnMapReadyCallback
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.createmarket_btn_create:
-                if (lat!=null&&lon!=null){
-
+                Log.d("CREATE MARKET.....","BUTTON CLICKED");
+                if (lat!=null&&lon!=null&&!et_name.getText().toString().equals("")){
+                    marketDataModel.setAdminID(userDataModelSingleTon.getId());
+                    marketDataModel.setName(et_name.getText().toString());
+                    marketDataModel.setLatitude(lat);
+                    marketDataModel.setLongitude(lon);
+                    marketDataModel.setAddress(address);
+                    marketDataModel.setImageURL("n/a");
+                    createMarket(marketDataModel);
+                }
+                else {
+                    Toast.makeText(this,"Data Incomplete",Toast.LENGTH_LONG).show();
                 }
                 break;
         }
+    }
+
+    private void createMarket(MarketDataModel marketDataModel){
+        markets=firebase.child("Markets").push();
+
+        markets.setValue(marketDataModel, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if (firebaseError!=null){
+                    Log.d("CREATE MARKET.....",firebaseError.getMessage());
+                    Toast.makeText(CreateMarket.this,"Error Creating Market",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    addMarkettoadmin(userDataModelSingleTon.getId(),markets.getKey());
+                }
+            }
+        });
+    }
+
+    private void addMarkettoadmin(String adminID, final String marketID){
+        HashMap<String,Object> hashMap=new HashMap<String, Object>();
+        hashMap.put("marketID",marketID);
+        firebase.child("Admin_Market").child(adminID).push().setValue(hashMap, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if (firebaseError!=null){
+                    Log.d("ADD MARKET TO ADMIN.....",firebaseError.getMessage());
+                    removeMarket();
+                }
+                else{
+                    Toast.makeText(CreateMarket.this,"Market Created",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void removeMarket(){
+        markets.removeValue(new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if(firebaseError!=null){
+                    Log.d("REMOVE MARKET.....",firebaseError.getMessage());
+                }
+                else {
+                    Toast.makeText(CreateMarket.this,"Error Creating Market",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/MarketDataModel.java
@@ -0,0 +1,53 @@
+package fuuast.fyp.fleamarket;
+
+public class MarketDataModel {
+    private String name, adminID, latitude, longitude, address, imageURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(String adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
