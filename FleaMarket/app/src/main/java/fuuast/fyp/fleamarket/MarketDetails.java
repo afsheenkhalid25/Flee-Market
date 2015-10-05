4bac1d1b80fd8a87b944189d8c0993340b65a401
afsheenkhalid25 <afsheen.kewt@gmail.com>
2015-10-05 17:54:28 +0500

    Market Details activity in process

---
+++ FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/MarketDetails.java
@@ -0,0 +1,14 @@
+package fuuast.fyp.fleamarket;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MarketDetails extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_details);
+    }
+}
---
+++ FleaMarket/app/src/main/java/fuuast/fyp/fleamarket/MarketDetails_CustomAdapter.java
@@ -0,0 +1,70 @@
+package fuuast.fyp.fleamarket;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MarketDetails_CustomAdapter extends BaseAdapter
+{
+    Context context;
+    ArrayList shop_Name,shop_Owner,shop_Category,shop_imageUrl;
+    TextView shopName,shopOwner,shopCategory;
+    ImageView img;
+
+
+    public MarketDetails_CustomAdapter(Context context, ArrayList names, ArrayList owner, ArrayList category, ArrayList imageUrl) {
+        this.context = context;
+        this.shop_Name = names;
+        this.shop_Owner = owner;
+        this.shop_Category = category;
+        this.shop_imageUrl = imageUrl;
+    }
+
+    @Override
+    public int getCount() {
+        return shop_Name.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.marketdetails_listview,null);
+
+        shopName = (TextView) v.findViewById(R.id.marketdetails_tv_shopname);
+        shopOwner = (TextView) v.findViewById(R.id.marketdetails_tv_shopowner);
+        shopCategory = (TextView) v.findViewById(R.id.marketdetails_tv_shopcategory);
+
+        img = (ImageView) v.findViewById(R.id.marketdetails_img_shopdetails);
+        img.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context,MarketDetails.class);
+                context.startActivity(i);
+            }
+        });
+
+        shopName.setText(shop_Name.get(position).toString());
+        shopOwner.setText(shop_Owner.get(position).toString());
+        shopCategory.setText(shop_Category.get(position).toString());
+
+        return v;
+    }
+}
